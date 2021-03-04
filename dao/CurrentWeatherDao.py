481e14c6ea9dd6f316d4beb94298f5cc2c4483a6
chowsychoch <cho.cho@ucdconnect.ie>
2021-03-04 17:38:24 +0000

    Revised weather columns with sunset sunrise etc cols

--- dao/CurrentWeatherDao.py
+++ dao/CurrentWeatherDao.py
@@ -12,6 +12,20 @@ class weather_api:
 
     # TODO: filter useful column
     def __filter_Weather_v2(self, arr: dict):
+        if 'gust' not in arr['wind']:
+            d_gust = 0
+        else:
+            d_gust = arr['wind']['gust']
+        
+        if 'rain' not in arr:
+            d_rain = 0
+        else:
+            d_rain = arr['rain']['rain_1h']
+        if 'snow' not in arr:
+            d_snow = 0
+        else:
+            d_snow = arr['snow']['snow_1h']
+
         return{
             'lat': float(arr["coord"]['lat']),
             'lon': float(arr["coord"]['lon']),
@@ -22,18 +36,24 @@ class weather_api:
             'visibility': arr['visibility'],
             'wind_speed': float(arr['wind']['speed']),
             'wind_deg': float(arr['wind']['deg']),
+            'wind_gust': float(d_gust),
             'temperature': float(arr['main']['temp']),
             'feels_like': float(arr['main']['feels_like']),
             'temp_min': float(arr['main']['temp_min']),
             'temp_max': float(arr['main']['temp_max']),
             'pressure': float(arr['main']['pressure']),
             'humidity': float(arr['main']['humidity']),
+            'rain_1h': float(d_rain),
+            'snow_1h': float(d_snow),
+            'sunrise': datetime.datetime.fromtimestamp(arr['sys']['sunrise']),
+            'sunset': datetime.datetime.fromtimestamp(arr['sys']['sunset']),
             'description': arr['weather'][0]['main']
         }
 
     def insert_weather_to_db_forSingleDict(self, weather: dict):
         try:
             weather_column = self.__filter_Weather_v2(weather)
+            print('here',weather_column["wind_gust"])
             newWeather = Weather(
                 lat=weather_column["lat"],
                 lon=weather_column["lon"],
@@ -44,12 +64,17 @@ class weather_api:
                 visibility=weather_column["visibility"],
                 wind_speed=weather_column["wind_speed"],
                 wind_deg=weather_column["wind_deg"],
+                wind_gust=weather_column["wind_gust"],
                 temperature=weather_column["temperature"],
                 feels_like=weather_column["feels_like"],
                 temp_min=weather_column["temp_min"],
                 temp_max=weather_column["temp_max"],
                 pressure=weather_column["pressure"],
                 humidity=weather_column["humidity"],
+                rain_1h=weather_column["rain_1h"],
+                snow_1h=weather_column["snow_1h"],
+                sunrise=weather_column["sunrise"],
+                sunset=weather_column["sunset"],
                 description=weather_column["description"]
             )
             self.session.add(newWeather)
