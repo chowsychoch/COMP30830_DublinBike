558e2d109704bdab0f60bcec13328b3107f67523
chowsychoch <cho.cho@ucdconnect.ie>
2021-03-23 18:51:39 +0000

    New Feature: Info Box on map

--- flaskapp/static/js/index.js
+++ flaskapp/static/js/index.js
@@ -5,7 +5,6 @@ function initMap() {
         //call the get api to get all stations point first
         return response.json()
     }).then(data => {
-        // console.log("data: ", data);
         //add the map 
         map = new google.maps.Map(document.getElementById("map"), {
             //disable default clickable landmark
@@ -13,15 +12,34 @@ function initMap() {
             center: { lat: 53.347777, lng: -6.244239 },
             zoom: 14,
         });
-        //loop over result and pass it to marker
-        for (let item of data) {
-            new google.maps.Marker({
-                position: { lat: item['pos_lat'], lng: item['pos_lng'] },
-                map: map,
-            })
-        }
+        //print marker
+        printMarker(data)
     }).catch(err => {
         console.log("Map Err", err);
     })
+
+}
+function printMarker(data) {
+    //loop over result and pass it to marker
+    for (let item of data) {
+        //info box of each marker
+        const contentString = `
+        <div id="content">Name: ${item['name']}</div>
+        <div class="info">Address: ${item['address']}</div>
+        <div class="info">Available Bikes: ${item['available_bikes']}</div>
+        <div class="info">Bike stands: ${item['bike_stands']}</div>
+        `
+        const infoWindow = new google.maps.InfoWindow({
+            content: contentString
+        })
+
+        item = new google.maps.Marker({
+            position: { lat: item['pos_lat'], lng: item['pos_lng'] },
+            map: map,
+        })
+        item.addListener("click", () => {
+            infoWindow.open(map, item);
+        })
+    }
 
 }
